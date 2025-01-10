1bea4720de58c72139dfdbed66b9c2c54f793f6d
Anurag Pramanik <p.anurag@outlook.com>
2025-01-10 13:07:14 +0530

    feat(dotfiles): add public dotfiles

---
+++ README.md
@@ -0,0 +1,11 @@
+# Anurag's dotfiles
+
+**Warning**: Don’t blindly use my settings unless you know what that entails. Use at your own risk!
+
+## Contents
+
+- zsh config
+- tmux config
+- neovim config
+- git config
+- vscode config
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,70 @@
+-- local discipline = require("anurag.discipline")
+
+-- discipline.cowboy()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- use jj to exit insert mode
+keymap.set("i", "jj", "<ESC>")
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- Do things without affecting the registers
+keymap.set("n", "x", '"_x')
+keymap.set("n", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>P", '"0P')
+keymap.set("v", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>c", '"_c')
+keymap.set("n", "<Leader>C", '"_C')
+keymap.set("v", "<Leader>c", '"_c')
+keymap.set("v", "<Leader>C", '"_C')
+keymap.set("n", "<Leader>d", '"_d')
+keymap.set("n", "<Leader>D", '"_D')
+keymap.set("v", "<Leader>d", '"_d')
+keymap.set("v", "<Leader>D", '"_D')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+-- keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
+-- Disable continuations
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
+
+keymap.set("n", "<leader>r", function()
+  require("anurag.hsl").replaceHexWithHSL()
+end)
+
+keymap.set("n", "<leader>i", function()
+  require("anurag.lsp").toggleInlayHints()
+end)
